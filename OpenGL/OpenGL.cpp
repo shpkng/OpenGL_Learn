b652d208b23da60ab2ca7f118c9edc600dc2b459
shpkng <shpkng@gmail.com>
2020-01-02 16:24:13 +0800

    -

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -1,14 +1,15 @@
-﻿// OpenGL.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <vector>
+#include <list>
+#include "include/stb_image.h"
 
 using namespace std;
 
-void frame_buffer_size_callback(GLFWwindow* window, int width, int height);
+void onResizeWindow(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void setShader();
 
 float vertices[] = {
 	0.5f, 0.5f, 0.0f,   // 右上角
@@ -17,23 +18,17 @@ float vertices[] = {
 	-0.5f, 0.5f, 0.0f   // 左上角
 };
 
+float texCoords[] = {
+	.0f, .0f,
+	1.0f, .0f,
+	.5f, 1.0f
+};
+
 unsigned int indices[] = { // 注意索引从0开始! 
 	0, 1, 3, // 第一个三角形
 	1, 2, 3  // 第二个三角形
 };
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
 
 int main() {
 	glfwInit();
@@ -55,30 +50,9 @@ int main() {
 		return -1;
 	}
 
-	glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+	glfwSetFramebufferSizeCallback(window, onResizeWindow);
 
-	// creating the vertex shader
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	// creating the fragment shader
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	// link the shader program object
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	// delete shader objects
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	//setShader();
 
 	//// link vertices
 	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -105,13 +79,14 @@ int main() {
 	glPolygonMode(GL_BACK, GL_LINE);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
-
 		glClearColor(.2, .3, .3, 1);//RGBA 0-1
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -126,12 +101,56 @@ int main() {
 	return 0;
 }
 
-void frame_buffer_size_callback(GLFWwindow* window, int width, int height) {
+void onResizeWindow(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
+//处理鼠标和键盘的输入
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
+
+	//if(glfwGetMouseButton(window, 0) == )
+}
+
+// 设置着色器
+void setShader() {
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+
+	// creating the vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// creating the fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// link the shader program object
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// delete shader objects
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glUseProgram(shaderProgram);
 }
